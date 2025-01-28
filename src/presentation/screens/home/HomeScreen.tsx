7c98e108e4965fa2edd183aba17a6aa17eaa5d66
Maxi <maxi3230lmr@gmail.com>
2025-01-28 10:47:21 -0300

    Actualizacion de cache con useQuery

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import {StyleSheet, View} from 'react-native';
 import React from 'react';
-import {Text} from 'react-native-paper';
+import {FAB, Text, useTheme} from 'react-native-paper';
 import {getPokemons} from '../../../actions/pokemons';
 import {useInfiniteQuery, useQueryClient} from '@tanstack/react-query';
 import PokeballBG from '../../components/UI/PokeballBG';
@@ -8,10 +8,15 @@ import {FlatList} from 'react-native-gesture-handler';
 import {globalTheme} from '../../../config/theme/global-theme';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import PokemonCard from '../../components/pokemons/PokemonCard';
+import {StackScreenProps} from '@react-navigation/stack';
+import {RootStackParams} from '../../navigator/StackNavigator';
 
-export const HomeScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'HomeScreen'> {}
+
+export const HomeScreen = ({navigation}: Props) => {
   const queryClient = useQueryClient();
   const {top} = useSafeAreaInsets();
+  const theme = useTheme();
 
   // Forma tradicional de peticion http
 
@@ -51,6 +56,14 @@ export const HomeScreen = () => {
         onEndReachedThreshold={0.6}
         onEndReached={() => fetchNextPage()}
         showsVerticalScrollIndicator={false}
+      />
+
+      <FAB
+        label="Buscar"
+        style={[globalTheme.fab, {backgroundColor: theme.colors.primary}]}
+        mode="elevated"
+        color={theme.dark ? 'black' : 'white'}
+        onPress={() => navigation.push('SearchScreen')}
       />
     </View>
   );
--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -1,10 +1,77 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, FlatList} from 'react-native';
+import React, {useMemo, useState} from 'react';
+import {globalTheme} from '../../../config/theme/global-theme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {ActivityIndicator, Text, TextInput} from 'react-native-paper';
+import PokemonCard from '../../components/pokemons/PokemonCard';
+import {Pokemon} from '../../../domain/entities/Pokemon';
+import {useQuery} from '@tanstack/react-query';
+import {
+  getPokemonNamesWithId,
+  getPokemonsByIds,
+} from '../../../actions/pokemons';
+import FullScreenLoader from '../../components/UI/FullScreenLoader';
 
-export const SearchScreen=() =>{
+export const SearchScreen = () => {
+  const {top} = useSafeAreaInsets();
+  const [term, setTerm] = useState('');
+
+  const {isLoading, data: pokemonNameList = []} = useQuery({
+    queryKey: ['pokemons', 'all'],
+    queryFn: () => getPokemonNamesWithId(),
+  });
+
+  const pokemonNameIdList = useMemo(() => {
+    //Si es un numero
+    if (!isNaN(Number(term))) {
+      const pokemon = pokemonNameList.find(
+        pokemon => pokemon.id === Number(term),
+      );
+      return pokemon ? [pokemon] : [];
+    }
+
+    if (term.length === 0) return [];
+
+    if (term.length < 3) return [];
+
+    return pokemonNameList.filter(pokemon =>
+      pokemon.name.includes(term.toLocaleLowerCase()),
+    );
+  }, [term]);
+
+  const {isLoading: isLoadingPokemons, data: pokemons = []} = useQuery({
+    queryKey: ['pokemons', 'by', pokemonNameIdList],
+    queryFn: () =>
+      getPokemonsByIds(pokemonNameIdList.map(pokemon => pokemon.id)),
+    staleTime: 1000 * 60 * 5, // 5 minutos
+  });
+
+  if (isLoading) {
+    return <FullScreenLoader />;
+  }
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={[globalTheme.globalMargin, {paddingTop: top + 10}]}>
+      <TextInput
+        placeholder="Buscar Pokemon"
+        mode="flat"
+        autoFocus
+        autoCorrect={false}
+        onChangeText={setTerm}
+        value={term}
+      />
+
+      {isLoadingPokemons && <ActivityIndicator style={{paddingTop: 20}} />}
+
+      <FlatList
+        data={pokemons}
+        keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
+        numColumns={2}
+        style={{paddingTop: top + 20}}
+        renderItem={({item}) => <PokemonCard pokemon={item} />}
+        showsVerticalScrollIndicator={false}
+        ListFooterComponent={<View style={{height: 150}} />}
+      />
     </View>
-  )
-}
+  );
+};
